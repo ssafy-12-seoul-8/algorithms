fc42ef79f76e4b9217f6346ea8f2ca1756c07745
Namju So <79499733+NJ97S@users.noreply.github.com>
2024-08-09 11:53:29 +0900

    [D4] Title: 러시아 국기 같은 깃발, Time: 131 ms, Memory: 18,356 KB -SsafyHub

--- 이하경/SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
+++ 이하경/SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
@@ -1,10 +1,21 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Solution {
 	public static int n, m, min, sum;
-	public static char[][] flag;
+	public static int[][] flag, memoColorIn;
+	public static int[] dColor = { 0, 1 };
+	public static Map<Character, Integer> map = new HashMap<>();
+	
+	static {
+		map.put('W', 0);
+		map.put('B', 1);
+		map.put('R', 2);
+	}
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,18 +30,24 @@ public class Solution {
 			n = Integer.parseInt(st.nextToken());
 			m = Integer.parseInt(st.nextToken());
 			
-			flag = new char[n][m];
+			flag = new int[n][m];
 			
+			// W는 0, B는 1, R은 2
 			for (int i = 0; i < n; i++) {
 				String input = br.readLine();
 				for (int j = 0; j < m; j++) {
-					flag[i][j] = input.charAt(j);;
+					flag[i][j] = map.get(input.charAt(j));
 				}
+			}
+
+			memoColorIn = new int[n][3];
+			for (int[] memo : memoColorIn) {
+				Arrays.fill(memo, -1);
 			}
 	
 			min = 50 * 50; // 전체 채색하는경우가 최대
-			sum = colorIn(0, 'W') + colorIn(n - 1, 'R');
-			backtracking(1, 'W');
+			sum = colorIn(0, 0) + colorIn(n - 1, 2);
+			backtracking(1, 0);
 			
 			sb.append("#").append(t).append(" ").append(min).append("\n");
 		}		
@@ -39,53 +56,42 @@ public class Solution {
 	}
 
 	// row번째 줄을 color색으로 칠할 때 몇개를 바꿔야하는지
-	public static int colorIn(int row, char color) {
+	public static int colorIn(int row, int color) {
+		if (memoColorIn[row][color] != -1) {
+			return memoColorIn[row][color];
+		}
 		int cnt = 0;
-		for (char c : flag[row]) {
+		for (int c : flag[row]) {
 			if (c != color) {
 				cnt++;
 			}
 		}
+		memoColorIn[row][color] = cnt;
 		return cnt;
 	}
 	
 	// 현재 탐색할 줄과 이전에 칠한 색을 받는다
 	// 현재 줄은 이전이 W -> W, B / 이전이 B -> B, R / 이전이 R -> R 로 칠할수있다
-	public static void backtracking(int row, char prevColor) {
+	public static void backtracking(int row, int prevColor) {
 		if (row == n - 1) { // 마지막 줄에 오면 끝
 			min = Math.min(min, sum);
 			return;
 		}
-		if (row == n - 2 && prevColor == 'W') { // 아래에서 두번째인데 B가 안나온경우 B로 칠한다
-			int cnt = colorIn(n - 2, 'B');
+		if (row == n - 2 && prevColor == 0) { // 아래에서 두번째인데 B가 안나온경우 B로 칠한다
+			int cnt = colorIn(n - 2, 1);
 			sum += cnt;
-			backtracking(n - 1, 'B');
-			sum -= cnt;
-		} else if (prevColor == 'W') {
-			int cnt = colorIn(row, 'W');
-			sum += cnt;
-			backtracking(row + 1, 'W');
-			sum -= cnt;
-			
-			cnt = colorIn(row, 'B');
-			sum += cnt;
-			backtracking(row + 1, 'B');
-			sum -= cnt;
-		} else if (prevColor == 'B') {
-			int cnt = colorIn(row, 'B');
-			sum += cnt;
-			backtracking(row + 1, 'B');
-			sum -= cnt;
-			
-			cnt = colorIn(row, 'R');
-			sum += cnt;
-			backtracking(row + 1, 'R');
+			backtracking(n - 1, 1);
 			sum -= cnt;
 		} else {
-			int cnt = colorIn(row, 'R');
-			sum += cnt;
-			backtracking(row + 1, 'R');
-			sum -= cnt;
+			for (int i = 0; i < 2; i++) {
+				int newColor = prevColor + dColor[i];
+				if (newColor < 3) {
+					int cnt = colorIn(row, newColor);
+					sum += cnt;
+					backtracking(row + 1, newColor);
+					sum -= cnt;
+				}
+			}	
 		}
 	}
 }
