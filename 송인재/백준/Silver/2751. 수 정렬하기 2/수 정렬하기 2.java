117d7bdd33276231963d25e6836d174b7a40f8b0
Injae Song <110002292+IjjS@users.noreply.github.com>
2024-08-21 10:54:45 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1408 ms, Memory: 113752 KB -SsafyHub

---
+++ 송인재/백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+        
+        Arrays.sort(nums);
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < n; i++) {
+            sb.append(nums[i])
+                .append("\n");
+        }
+        
+        System.out.println(sb);
+    }
+    
+}
