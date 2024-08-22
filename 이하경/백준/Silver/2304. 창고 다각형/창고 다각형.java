5bcb8a652f3004b2ae796f8ec9ea61328ee8d0f8
puranium <63571947+puranium235@users.noreply.github.com>
2024-08-23 08:07:30 +0900

    [Silver II] Title: 창고 다각형, Time: 72 ms, Memory: 11976 KB -SsafyHub

--- 이하경/백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 이하경/백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -1,54 +1,39 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-    public static void main(String args[])throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[1001];
-        
-        int maxL = 0;
-        for (int i = 0; i < n; i++) {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+		int[] box = new int[1001];
+
+		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			int l = Integer.parseInt(st.nextToken());
 			int h = Integer.parseInt(st.nextToken());
-			arr[l] = h;
-			maxL = Math.max(maxL, l);
+			box[l] = h;
 		}
-        
-        int max = 0;
-        int maxIdxStart = 0;
-        int maxIdxEnd = maxL;
-        int cnt = 0;
-        
-        // 최댓값보다 앞부분의 넓이
-        for (int i = 0; i <= maxL; i++) {
-        	if (arr[i] > max) {
-        		cnt += max * (i - maxIdxStart);
-        		max = arr[i];
-        		maxIdxStart = i;
-        	}
-        }
 
-        // 최댓값보다 뒷부분의 넓이
-        max = 0;
-        for (int i = maxL; i >= maxIdxStart; i--) {
-			if (arr[i] > max) {
-				cnt += max * (maxIdxEnd - i);
-				max = arr[i];
-				maxIdxEnd = i;
+		int left = 1, right = 1000, leftMax = box[1], rightMax = box[1000];
+		int ans = 0;
+
+		while (left <= right) {
+			if (leftMax > rightMax) {
+				ans += rightMax;
+				right--;
+				rightMax = Math.max(rightMax, box[right]);
+			} else {
+				ans += leftMax;
+				left++;
+				leftMax = Math.max(leftMax, box[left]);
 			}
 		}
 
-        // 가장 큰 기둥의 넓이
-        cnt += max * (maxIdxEnd - maxIdxStart + 1);
-        
-        System.out.println(cnt);
-    }
-   
+		System.out.println(ans);
+
+	}
+
 }
