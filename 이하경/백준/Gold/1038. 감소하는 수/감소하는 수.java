ea2ab975a44f946230b980db05a6d6e70c1b9851
puranium <63571947+puranium235@users.noreply.github.com>
2024-08-24 16:29:24 +0900

    [Gold V] Title: 감소하는 수, Time: 68 ms, Memory: 11664 KB -SsafyHub

--- 이하경/백준/Gold/1038. 감소하는 수/감소하는 수.java
+++ 이하경/백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -1,60 +1,34 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
+    static ArrayList<Long> decNums = new ArrayList<>();
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
-        if (n >= 1023) {
+        if (n > 1022) {
             System.out.println(-1);
             return;
         }
 
-        if (n == 1022) {
-            System.out.println(9876543210L);
-            return;
+        for (int i = 0; i < 10; i++) {
+            makeDecNum(i);
         }
-
-        if (n >= 1012) {
-            int[] num = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-            int exclude = n - 1012;
-            String ans = "";
-            for (int i = 0; i < 10; i++) {
-                if (i != exclude) {
-                    ans += num[i];
-                }
-            }
-            System.out.println(ans);
-            return;
-        }
-
-
-        int cnt = -1;
-        for (long i = 0; i < 1e10; i++) {
-            if (isDecreasing(i)) {
-                cnt++;
-            }
-            if (cnt == n) {
-                System.out.println(i);
-                break;
-            }
-        }
-
+        Collections.sort(decNums);
+        System.out.println(decNums.get(n));
     }
 
-    static boolean isDecreasing(long k) {
-        int before = -1;
-        while (k != 0){
-            if ((int) (k % 10) > before) {
-                before = (int) (k % 10);
-                k /= 10;
-            } else {
-                return false;
-            }
+    // k의 오른쪽에 솟자를 추가해서 감소하는 수를 만든다
+    static void makeDecNum (long k) {
+        decNums.add(k);
+        int onesPlace = (int) (k % 10);
+        for (int i = 0; i < onesPlace; i++) {
+            makeDecNum(k * 10 + i);
         }
-        return true;
     }
 }
