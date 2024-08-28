89ce1befd4001ac2071cb5ad2f319e0324d38279
sybang97 <sybang98@gmail.com>
2024-08-28 23:14:47 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 169 ms, Memory: 24,808 KB -SsafyHub

--- 방승윤/SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ 방승윤/SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -1,85 +1,80 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Stack;
 import java.util.StringTokenizer;
-
+ 
 public class Solution {
-    
-    static int N, K, minDiff;
+     
+    static int N, minDiff, cntA, cntB;
     static int[][] ingredient;
-    static Stack<Integer> A;
-    static Stack<Integer> B;
-
+    static int[] ingredientNums;
+ 
     public static void main(String[] args) throws Exception {
-        
+         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
         int T = Integer.parseInt(st.nextToken());
-        
+         
         for (int test_case = 1; test_case <= T; test_case++) {
-            
+             
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
-            A = new Stack<>();
-            B = new Stack<>();
-            
+             
             ingredient = new int [N][N];
-            
+            ingredientNums = new int[N];
+             
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++) {
                     ingredient[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-            
+             
             minDiff = Integer.MAX_VALUE;
-            
+            cntA = 0;
+            cntB = 0;
+             
             recur(0, 0, 0);
-            
+             
             sb.append("#").append(test_case).append(" ").append(minDiff).append("\n");
-            
+             
         }
         System.out.println(sb);
     }
-    
+     
     static void recur(int idx, int tasteA, int tasteB) {
         if (idx == N) {
             minDiff = Math.min(minDiff, Math.abs(tasteA - tasteB));
             return;
         }
-        
-        if (A.size() < N / 2) {
-            A.push(idx);
-            
+         
+        if (cntA < N / 2) {
             int temp = 0;
-            
-            for (int i = 0; i < A.size(); i++) {
-                temp += ingredient[A.get(i)][idx];
-                temp += ingredient[idx][A.get(i)];
+            ingredientNums[cntA++] = idx;
+             
+            for (int i = 0; i < cntA; i++) {
+                temp += ingredient[ingredientNums[i]][idx];
+                temp += ingredient[idx][ingredientNums[i]];
             }
-            
+             
             recur(idx + 1, tasteA + temp, tasteB);
-            
-            A.pop();
+             
+            cntA--;
         }
-        
-        if (B.size() < N / 2) {
-            B.push(idx);
-            
+         
+        if (cntB < N / 2) {         
             int temp = 0;
-            
-            for (int i = 0; i < B.size(); i++) {
-                temp += ingredient[B.get(i)][idx];
-                temp += ingredient[idx][B.get(i)];
+            ingredientNums[N / 2 + cntB++] = idx;
+             
+            for (int i = N / 2; i < N / 2 + cntB; i++) {
+                temp += ingredient[ingredientNums[i]][idx];
+                temp += ingredient[idx][ingredientNums[i]];
             }
-            
+             
             recur(idx + 1, tasteA, tasteB + temp);
-            
-            B.pop();
+             
+            cntB--;
         }
     }
-    
+     
 }
