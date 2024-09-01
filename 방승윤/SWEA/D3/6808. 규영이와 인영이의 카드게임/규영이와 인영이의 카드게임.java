a8ec2f4ffb862276555267a69ee7515cc379dfa8
sybang97 <sybang98@gmail.com>
2024-09-01 18:10:49 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 1,169 ms, Memory: 19,912 KB -SsafyHub

--- 방승윤/SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ 방승윤/SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -1,78 +1,80 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
 	
+	static int card, cnt;
 	static int total = 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2;
-	static int cnt, temp;
-	static int[] A = new int[9];
-	static int[] B = new int[9];
-	
-	public static void main(String[] args) throws Exception {
+    
+    public static void main(String[] args) throws Exception {
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(st.nextToken());
-		
-		for (int tc = 1; tc <= T; tc++) {
-			
-			st = new StringTokenizer(br.readLine());
-			
-			for (int i = 0; i < 9; i++) {
-				B[i] = Integer.parseInt(st.nextToken());
-			}
-			
-			Arrays.sort(B);
-			for (int i = 0; i < 9; i++) {
-				A[i] = B[8 - i];
-			}
-			
-			int idxB = 0;
-			check : for (int i = 1; i <= 18; i++) {
-				for (int j = 0; j < 9; j++) {
-					if (A[j] == i) {
-						continue check;
-					}
-				}
-				B[idxB++] = i;
-			}
-			
-			cnt = 0;
-			winCnt(-1, 0, 0, 0);
-			
-			sb.append("#").append(tc).append(" ").append(total - cnt).append(" ").append(cnt).append("\n");
-			
-		}
-		System.out.println(sb);
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	StringBuffer sb = new StringBuffer();
+    	
+    	int T = Integer.parseInt(st.nextToken());
+    	
+    	for (int tc = 1; tc <= T; tc++) {
+    		
+    		st = new StringTokenizer(br.readLine());
+    		
+    		for (int i = 0; i < 9; i++) {
+    			card += (1 << Integer.parseInt(st.nextToken()) - 1);
+    		}
+    		
+    		game(-1, 0, 0, 0);
+    				
+    		sb.append("#").append(tc).append(" ").append(cnt).append(" ").append(total - cnt).append("\n");
+    		
+    		card = 0; 
+    		cnt = 0;
+    		
+    	}
+    	
+    	System.out.println(sb);
+    	
 	}
-	
-	static void winCnt(int idx, int scoreA, int scoreB, int check) {	
-		
-		if (idx != -1 && A[idx] > temp) {
-			scoreA += A[idx] + temp;
-		} else if (idx != -1) {
-			scoreB += A[idx] + temp;
-		}
-		
-		if (scoreA > 85) {
-			return;
-		}
-		
-		if (idx == 8) {
-			if (scoreB > scoreA) {
-				cnt++;
-			}
-			return;
-		}
-		
-		for (int i = 0; i < 9; i++) {
-			if ((check & (1 << i)) == 0) {
-				temp = B[i];
-				winCnt(idx + 1, scoreA, scoreB, check + (1 << i));
-			}
-		}
-	}
+    
+    static void game(int idx, int sidx, int lose, int card2) {
+    	
+    	int check = 0;
+    	int card1 = 0;
+    	
+    	if (idx != -1) {
+    		for (int i = 0; i < 18; i++) {
+        		if ((card & (1 << i)) > 0) {
+        			if (check == idx) {
+        				card1 = i + 1;
+        				break;
+        			}
+        			check++;
+        		}
+        	}
+    	}
+    	
+    	check = 0;
+    	
+    	if (card2 > card1) {
+    		lose += card1 + card2;
+    	}
+    	
+    	if (lose > 85) {
+    		return;
+    	}
+    	
+    	if (idx == 8) {
+			cnt++;
+    		return;
+    	}
+    	
+    	for (int i = 0; i < 18; i++) {
+    		if ((card & (1 << i)) > 0 || (sidx & (1 << i)) > 0) {
+    			continue;
+    		}
+			game(idx + 1, sidx + (1 << i), lose, i + 1);
+    	}
+    
+    }
     
 }
