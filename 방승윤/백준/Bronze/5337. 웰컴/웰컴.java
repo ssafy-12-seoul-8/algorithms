9f057ced90ae12c2b4cf711679aca3188f938df7
sybang97 <sybang98@gmail.com>
2024-09-07 01:46:34 +0900

    [Bronze V] Title: 웰컴, Time: 64 ms, Memory: 11196 KB -SsafyHub

---
+++ 방승윤/백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(".  .   .\n").append("|  | _ | _. _ ._ _  _\n").append("|/\\|(/.|(_.(_)[ | )(/.");
+		
+		System.out.println(sb);
+	}
+	
+}
