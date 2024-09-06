a2d331efe4393720de6f43ed26471de37948cf65
sybang97 <sybang98@gmail.com>
2024-09-07 01:50:04 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 60 ms, Memory: 11328 KB -SsafyHub

---
+++ 방승윤/백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("       _.-;;-._\n").append("'-..-'|   ||   |\n").append("'-..-'|_.-;;-._|\n").append("'-..-'|   ||   |\n").append("'-..-'|_.-''-._|");
+		
+		System.out.println(sb);
+	}
+	
+}
