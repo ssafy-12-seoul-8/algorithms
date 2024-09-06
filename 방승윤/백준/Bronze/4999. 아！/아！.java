be96e21aee547c6bb12ae0964811f732c748c116
sybang97 <sybang98@gmail.com>
2024-09-07 01:47:33 +0900

    [Bronze V] Title: 아!, Time: 64 ms, Memory: 11476 KB -SsafyHub

---
+++ 방승윤/백준/Bronze/4999. 아！/아！.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println(br.readLine().length() >= br.readLine().length() ? "go" : "no");
+		
+	}
+	
+}
