edafce7a07a16e6ee1e5be37625e08fc4af827cf
sybang97 <sybang98@gmail.com>
2024-09-07 02:00:25 +0900

    [Bronze V] Title: 카드 게임, Time: 64 ms, Memory: 11492 KB, Score: 100 point -SsafyHub

---
+++ 방승윤/백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int cnt = 0;
+		
+		for (int i = 0; i < 5; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			cnt += Integer.parseInt(st.nextToken());
+		}
+		
+		System.out.println(cnt);
+		
+	}
+	
+}
