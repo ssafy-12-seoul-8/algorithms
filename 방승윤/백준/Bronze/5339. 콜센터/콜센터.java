dd3a62fb2e1b35e9ab9eb52d1e582a96cbbf3865
sybang97 <sybang98@gmail.com>
2024-09-07 01:54:28 +0900

    [Bronze V] Title: 콜센터, Time: 56 ms, Memory: 11204 KB -SsafyHub

---
+++ 방승윤/백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("     /~\\\n").append("    ( oo|\n").append("    _\\=/_\n").append("   /  _  \\\n").append("  //|/.\\|\\\\\n").append(" ||  \\ /  ||\n").append("============\n").append("|          |\n").append("|          |\n").append("|          |");
+
+		System.out.println(sb);
+		
+	}
+	
+}
