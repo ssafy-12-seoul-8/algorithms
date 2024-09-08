81b7d9fad8cfbcc1c8e77d91722189142633fc24
puranium <63571947+puranium235@users.noreply.github.com>
2024-09-08 14:29:33 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 492 ms, Memory: 122904 KB -SsafyHub

---
+++ 이하경/백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -0,0 +1,90 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[][] map;
+    static int[] dr = { 1, 0, -1, 0 };
+    static int[] dc = { 0, 1, 0, -1 };
+
+    public static void main(String[] args) throws Exception {
+        input();
+        System.out.println(bfs());
+    }
+
+    static void input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            String inp = br.readLine();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = inp.charAt(j) - '0';
+            }
+        }
+    }
+
+    static int bfs() {
+        if (n == 1 && m == 1) {
+            return 1;
+        }
+
+        boolean[][] visitBefore = new boolean[n][m];
+        boolean[][] visitAfter = new boolean[n][m];
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(0, 0, 1, true));
+        visitBefore[0][0] = true;
+
+        while(!q.isEmpty()) {
+            Point curr = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nr = curr.r + dr[i];
+                int nc = curr.c + dc[i];
+
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m && !visitBefore[nr][nc]) {
+                    if (nr == n - 1 && nc == m - 1) {
+                        return curr.dist + 1;
+                    }
+
+                    if (map[nr][nc] == 1 && curr.canBreak && !visitAfter[nr][nc]) { // 벽일때
+                        visitAfter[nr][nc] = true;
+                        q.add(new Point(nr, nc, curr.dist + 1, false));
+                        continue;
+                    }
+
+                    if (map[nr][nc] == 0 && curr.canBreak) {
+                        visitBefore[nr][nc] = true;
+                        q.add(new Point(nr, nc, curr.dist + 1, true));
+                        continue;
+                    }
+
+                    if (map[nr][nc] == 0 && !visitAfter[nr][nc] && !curr.canBreak) {
+                        visitAfter[nr][nc] = true;
+                        q.add(new Point(nr, nc, curr.dist + 1, false));
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    static class Point {
+        int r, c, dist;
+        boolean canBreak;
+
+        public Point(int r, int c, int dist, boolean canBreak) {
+            this.r = r;
+            this.c = c;
+            this.dist = dist;
+            this.canBreak = canBreak;
+        }
+    }
+}
