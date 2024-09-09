6fd2388c67fbfa760708da01c71ed048e39c923b
sybang97 <sybang98@gmail.com>
2024-09-09 16:40:19 +0900

    [D4] Title: 치즈 도둑, Time: 338 ms, Memory: 33,008 KB -SsafyHub

--- 방승윤/SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
+++ 방승윤/SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
@@ -1,82 +1,78 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
- 
-public class Solution {
-     
-    static int N;
-    static int[][] board;
-    static boolean[][] visit;
-     
-    public static void main(String[] args) throws Exception {
-         
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int T = Integer.parseInt(st.nextToken());
-         
-        for (int tc = 1; tc <= T; tc++) {
-             
-            st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            board = new int[N][N];
-            int lastDay = 0;
-            
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
-                	board[i][j] = Integer.parseInt(st.nextToken());
-                	lastDay = Math.max(lastDay, board[i][j]);
-                }
-            }
-            
-            if (lastDay > 100) {
-            	lastDay = 100;
-            }
-            
-            int maxCnt = 1;
-             
-            for (int day = 1; day <= lastDay; day++) {
-            	visit = new boolean[N][N];
-            	
-            	int cnt = 0;
-            	
-            	for (int r = 0; r < N; r++) {
-                	for (int c = 0; c < N; c++) {
-                		if (visit[r][c] || board[r][c] <= day) {
-                			continue;
-                		}
-                		visit[r][c] = true;
-                		dfs(day, r, c);
-                		cnt++;
-                	}
-                }
-            	
-            	maxCnt = Math.max(maxCnt, cnt);
-            	
-            }
-            
-            sb.append("#").append(tc).append(" ").append(maxCnt).append("\n");
-            
-        }
-         
-        System.out.println(sb);
-         
-    }
-    
-    static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, 1, 0, -1};
-     
-    static void dfs(int day, int r, int c) {
 
-    	for (int i = 0; i < 4; i++) {
-    		int nr = r + dr[i];
-    		int nc = c + dc[i];
-    		if (nr >= 0 && nr < N && nc >= 0 && nc < N && !visit[nr][nc] && board[r][c] > day) {
-    			visit[nr][nc] = true;
-    			dfs(day, nr, nc);
-    		}
-    	}
-    	
-    }    
+class Solution {
+	
+	static int piece, N, day;
+	static int[][] cheeze;
+	static boolean[][] visited;
+	static int[] dr = {-1, 0, 1, 0};
+	static int[] dc = {0, 1, 0, -1};
+	
+	public static void main(String[] args) throws Exception {
+		
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int T = Integer.parseInt(st.nextToken());
+		
+		for (int tc = 1; tc <= T; tc++) {
+			
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken()); 
+			cheeze = new int[N][N];
+			piece = 1;
+			int maxDay = 1;
+			
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					cheeze[i][j] = Integer.parseInt(st.nextToken());
+					maxDay = Math.max(maxDay, cheeze[i][j]);
+				}
+			}
+			
+			for (day = 1; day < maxDay; day++) {
+				visited = new boolean[N][N];
+				int temp = 0;
+				
+				for (int i = 0; i < N; i++) {
+					for (int j = 0; j < N; j++) {
+						if (!visited[i][j] && cheeze[i][j] > day) {
+							dfs(i, j);
+							temp++;
+						}
+					}
+				}
+				
+				piece = Math.max(piece, temp);
+				
+			}
+			
+			sb.append("#").append(tc).append(" ").append(piece).append("\n");
+			
+		}
+		
+		System.out.println(sb);
+		
+	}
+	
+	static void dfs(int r, int c) {
+		
+		visited[r][c] = true;
+		
+		for (int i = 0; i < 4; i++) {
+			int nr = r + dr[i];
+			int nc = c + dc[i];
+			
+			if (nr >= 0 && nr < N && nc >= 0 && nc < N && !visited[nr][nc] && cheeze[nr][nc] > day) {
+				dfs(nr, nc);
+			}
+		}
+		
+		
+	}
+	
 }
