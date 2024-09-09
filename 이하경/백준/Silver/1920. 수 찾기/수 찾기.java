f58dbe149cbb5533444fde66357bf35e71fb5a50
puranium <63571947+puranium235@users.noreply.github.com>
2024-09-09 17:46:23 +0900

    [Silver IV] Title: 수 찾기, Time: 404 ms, Memory: 50568 KB -SsafyHub

--- 이하경/백준/Silver/1920. 수 찾기/수 찾기.java
+++ 이하경/백준/Silver/1920. 수 찾기/수 찾기.java
@@ -4,13 +4,15 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int[] nums;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 
 		int n = Integer.parseInt(br.readLine());
-		int[] nums = new int[n];
+		nums = new int[n];
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
 			nums[i] = Integer.parseInt(st.nextToken());
@@ -21,9 +23,24 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < m; i++) {
 			int target = Integer.parseInt(st.nextToken());
-			sb.append(Arrays.binarySearch(nums, target) >= 0 ? 1 : 0).append("\n");
+			sb.append(binarySearch(0, n - 1, target) >= 0 ? 1 : 0).append("\n");
 		}
 
 		System.out.println(sb);
+	}
+
+	static int binarySearch(int start, int end, int target) {
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			if (nums[mid] == target) {
+				return mid;
+			}
+			if (nums[mid] > target) {
+				end = mid - 1;
+				continue;
+			}
+			start = mid + 1;
+		}
+		return -1;
 	}
 }
