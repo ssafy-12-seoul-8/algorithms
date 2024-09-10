f067a5890a604b8128c6960c46a111972113ea97
sybang97 <sybang98@gmail.com>
2024-09-10 23:46:08 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 64 ms, Memory: 11516 KB -SsafyHub

---
+++ 방승윤/백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		System.out.print(N / 5 + N / 25 + N / 125);
+		
+	}
+	
+}
