b7c153c311581ca9547249dc002831dcf6bae086
puranium <63571947+puranium235@users.noreply.github.com>
2024-09-11 15:26:56 +0900

    [Gold IV] Title: N-Queen, Time: 1104 ms, Memory: 11572 KB -SsafyHub

--- 이하경/백준/Gold/9663. N－Queen/N－Queen.java
+++ 이하경/백준/Gold/9663. N－Queen/N－Queen.java
@@ -3,48 +3,46 @@ import java.io.InputStreamReader;
 
 public class Main {
 	static int n, cnt;
-	static int[] queen;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		n = Integer.parseInt(br.readLine());
 		cnt = 0;
-		queen = new int[n];
 		for (int i = 0; i < n / 2; i++) {
-			queen[0] = i;
-			nQueen(1);
+			nQueen(1, 1 << i << 1, 1 << i, 1 << i >> 1);
 		}
 		cnt *= 2;
 		if (n % 2 == 1) {
-			queen[0] = n / 2;
-			nQueen(1);
+			nQueen(1, 1 << n / 2 << 1, 1 << n / 2, 1 << n / 2 >> 1);
 		}
 
 		System.out.println(cnt);
 	}
 
-	static void nQueen(int depth) {
+	// bit에는 이번에 못놓는 곳이 표시된다
+	static void nQueen(int depth, int leftBit, int midBit, int rightBit) {
 		if (depth == n) {
 			cnt++;
 			return;
 		}
+
 		for (int i = 0; i < n; i++) {
-			if (isValid(depth, i)) {
-				queen[depth] = i;
-				nQueen(depth + 1);
+			if (isValid(i, leftBit, midBit, rightBit)) {
+				nQueen(depth + 1, (leftBit | 1 << i) << 1, midBit | 1 << i, (rightBit | 1 << i) >> 1);
 			}
 		}
 	}
 
-	static boolean isValid(int d, int c) {
-		for (int i = 0; i < d; i++) {
-			if (queen[i] == c) {
-				return false;
-			}
-			if (Math.abs(c - queen[i]) == d - i) {
-				return false;
-			}
+	static boolean isValid(int shift, int leftBit, int midBit, int rightBit) {
+		if (((1 << shift) & leftBit) != 0) {
+			return false;
+		}
+		if (((1 << shift) & midBit) != 0) {
+			return false;
+		}
+		if (((1 << shift) & rightBit) != 0) {
+			return false;
 		}
 		return true;
 	}
