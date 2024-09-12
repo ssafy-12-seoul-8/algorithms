201d4b05b0e24dae2c0bdadf10dd7e657d2a74c5
sybang97 <sybang98@gmail.com>
2024-09-12 19:49:56 +0900

    [D3] Title: 햄버거 다이어트, Time: 161 ms, Memory: 20,576 KB -SsafyHub

--- 방승윤/SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ 방승윤/SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,56 +1,39 @@
-import java.util.*;
-import java.io.*;
- 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Solution {
-     
-    static int N, L, maxScore;
-    static int[][] ingredient;
-     
-    public static void main(String[] args) throws Exception {
-         
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        int T = Integer.parseInt(br.readLine());
-         
-        for (int test_case = 1; test_case <= T; test_case++) {
-             
-            st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            L = Integer.parseInt(st.nextToken());
-            ingredient = new int[N][2];
- 
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                ingredient[i][0] = Integer.parseInt(st.nextToken());
-                ingredient[i][1] = Integer.parseInt(st.nextToken());
-            }
-             
-            findScore(0, 0, 0);
-             
-            sb.append("#").append(test_case).append(" ").append(maxScore).append("\n");
-            maxScore = 0;
-        }
-        System.out.println(sb);
-    }
-     
-    static void findScore(int idx, int score, int calorie) {
-    	if (calorie > L) {
-            return;
-        }
-    	
-    	if (calorie <= L && score > maxScore) {
-            maxScore = score;
-        }
-    	
-        if (idx == N) {
-            return;
-        }
-     
-        for (int i = idx; i < N; i++) {
-        	findScore(i + 1, score + ingredient[i][0], calorie + ingredient[i][1]);
-        }
-        
-    }
-     
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int TC = Integer.parseInt(st.nextToken());
+		
+		for (int tc = 1; tc <= TC; tc++) {
+			
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int L = Integer.parseInt(st.nextToken());
+			int[] taste = new int[L + 1];
+			
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				int T = Integer.parseInt(st.nextToken());
+				int K = Integer.parseInt(st.nextToken());
+				
+				for (int j = L; j >= K; j--) {
+					taste[j] = Math.max(taste[j - K] + T, taste[j]);
+				}
+			}
+			
+			sb.append("#").append(tc).append(" ").append(taste[L]).append("\n");
+			
+		}
+		
+		System.out.println(sb);
+		
+	}
+	
 }
