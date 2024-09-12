8c4841da65f63ddb489c01ca48641ebfe2a3764e
puranium <63571947+puranium235@users.noreply.github.com>
2024-09-12 16:02:30 +0900

    [Silver IV] Title: 상근이의 여행, Time: 152 ms, Memory: 22856 KB -SsafyHub

---
+++ 이하경/백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int t = 0; t < T; t++) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+
+			for (int i = 0; i < m; i++) {
+				br.readLine();
+			}
+
+			sb.append(n - 1).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+}
