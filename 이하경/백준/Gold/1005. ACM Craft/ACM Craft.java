665adc79a33b76de4cdf5ff7c3fc36f4b65b559d
puranium <63571947+puranium235@users.noreply.github.com>
2024-09-16 20:49:24 +0900

    [Gold III] Title: ACM Craft, Time: 576 ms, Memory: 241972 KB -SsafyHub

--- 이하경/백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 이하경/백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -1,95 +1,62 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-    static int n, k, w;
-    static int[] delay, inDegree;
-    static ArrayList<Integer>[] edges;
+    static ArrayList<Integer>[] need;
+    static int[] memo, delay;
 
     public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
 
         for (int t = 0; t < T; t++) {
-            input();
-            solve();
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+            delay = new int[n + 1];
+            for (int i = 1; i <= n; i++) {
+                delay[i] = Integer.parseInt(st.nextToken());
+            }
+
+            need = new ArrayList[n + 1];
+            for (int i = 1; i <= n; i++) {
+                need[i] = new ArrayList<>();
+            }
+
+            for (int i = 0; i < k; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                need[y].add(x);
+            }
+
+            memo = new int[n + 1];
+            Arrays.fill(memo, -1);
+
+            int w = Integer.parseInt(br.readLine());
+            sb.append(time(w)).append("\n");
         }
         System.out.println(sb);
     }
 
-    static void input() throws Exception {
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine());
-        delay = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            delay[i] = Integer.parseInt(st.nextToken());
+    // k를 짓는데 걸리는 시간, 최대 1000 * 100,000
+    static int time(int k) {
+        if (memo[k] >= 0) {
+            return memo[k];
         }
 
-        edges = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) {
-            edges[i] = new ArrayList<>();
+        int maxTime = 0;
+        for (int prev : need[k]) {
+            maxTime = Math.max(maxTime, time(prev));
         }
-
-        inDegree = new int[n + 1];
-        for (int i = 0; i < k; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            edges[x].add(y);
-            inDegree[y]++;
-        }
-
-        w = Integer.parseInt(br.readLine());
-    }
-
-    static void solve() {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-
-        for (int i = 1; i <= n; i++) {
-            if (inDegree[i] == 0) {
-                if (i == w) {
-                    sb.append(delay[i]).append("\n");
-                    return;
-                }
-                pq.add(new Node(i, delay[i]));
-            }
-        }
-
-        while (!pq.isEmpty()) {
-            Node curr = pq.poll(); // 번호와, 거기까지 짓는데 걸린 시간 나옴
-
-            for (int next : edges[curr.v]) {
-                inDegree[next]--;
-
-                if (inDegree[next] == 0) {
-                    if (next == w) {
-                        sb.append(curr.wait + delay[next]).append("\n");
-                        return;
-                    }
-                    pq.add(new Node(next, curr.wait + delay[next]));
-                }
-            }
-        }
-    }
-
-    static class Node implements Comparable<Node> {
-        int v, wait;
-
-        public Node(int v, int wait) {
-            this.v = v;
-            this.wait = wait;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return this.wait - o.wait;
-        }
+        return memo[k] = maxTime + delay[k];
     }
 }
