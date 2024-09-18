50617686ea0e2bf278ffa80b292f188ae96965e2
puranium <63571947+puranium235@users.noreply.github.com>
2024-09-18 17:03:55 +0900

    [Gold II] Title: 공항, Time: 172 ms, Memory: 21548 KB -SsafyHub

--- 이하경/백준/Gold/10775. 공항/공항.java
+++ 이하경/백준/Gold/10775. 공항/공항.java
@@ -1,31 +1,42 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.TreeSet;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int g = Integer.parseInt(br.readLine());
         int p = Integer.parseInt(br.readLine());
 
-        TreeSet<Integer> gates = new TreeSet<>();
-
+        int[] parent = new int[g + 1]; // i보다 작거나같은 빈 곳
         for (int i = 1; i <= g; i++) {
-            gates.add(i);
+            parent[i] = i;
         }
 
-        int cnt = 0;
         for (int i = 0; i < p; i++) {
             int plane = Integer.parseInt(br.readLine());
-            Integer find = gates.floor(plane);
-            if (find == null) {
-                System.out.println(cnt);
+            int gate = find(parent, plane);
+            if (gate == 0) {
+                System.out.println(i);
                 return;
             }
-            gates.remove(find);
-            cnt++;
+            union(parent, gate - 1, gate);
         }
-        System.out.println(cnt);
+        System.out.println(p);
+    }
+
+    static void union(int[] p, int x, int y) {
+        int px = find(p, x);
+        int py = find(p, y);
+
+        p[py] = px;
+    }
+
+    static int find(int[] p, int x) {
+        if (p[x] != x) {
+            p[x] = find(p, p[x]);
+        }
+        return p[x];
     }
 }
