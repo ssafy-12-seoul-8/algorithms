a3b121f03ba5641166a46667e8c1685da1e9303d
puranium <63571947+puranium235@users.noreply.github.com>
2024-09-23 23:24:06 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 208 ms, Memory: 23836 KB -SsafyHub

--- 이하경/백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 이하경/백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,66 +10,20 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        char[][] map = new char[n][m];
-        for (int i = 0; i < n; i++) {
-            map[i] = br.readLine().toCharArray();
-        }
-
-        // 0번 행과 열 처리
-        int[][] rowLen = new int[n][m]; // 가로로 이어진 개수
-        int[][] colLen = new int[n][m]; // 세로로 이어진 개수
-        int[][] sqLen = new int[n][m];
-        rowLen[0][0] = map[0][0] - '0';
-        colLen[0][0] = map[0][0] - '0';
-        sqLen[0][0] = map[0][0] - '0';
-
-        for (int i = 1; i < m; i++) {
-            if (map[0][i] == '1') {
-                rowLen[0][i] = rowLen[0][i - 1] + 1;
-                colLen[0][i] = 1;
-                sqLen[0][i] = 1;
-            }
-        }
-
-        for (int i = 1; i < n; i++) {
-            if (map[i][0] == '1') {
-                rowLen[i][0] = 1;
-                colLen[i][0] = colLen[i - 1][0] + 1;
-                sqLen[i][0] = 1;
-            }
-        }
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (map[i][j] == '1') {
-                    rowLen[i][j] = rowLen[i][j - 1] + 1;
-                    colLen[i][j] = colLen[i - 1][j] + 1;
-                }
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            char[] inp = br.readLine().toCharArray();
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = inp[j - 1] - '0';
             }
         }
 
         int max = 0;
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                sqLen[i][j] = Math.min(rowLen[i][j], colLen[i][j]);
-                sqLen[i][j] = Math.min(sqLen[i][j], sqLen[i - 1][j - 1] + 1);
-                max = Math.max(max, sqLen[i][j]);
-            }
-        }
-
-        if (max == 0) {
-            for (int i = 0; i < m; i++) {
-                if (map[0][i] == '1') {
-                    max = 1;
-                    break;
-                }
-            }
-        }
-        if (max == 0) {
-            for (int i = 1; i < n; i++) {
-                if (map[i][0] == '1'){
-                    max = 1;
-                    break;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (dp[i][j] == 1) {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                    max = Math.max(max, dp[i][j]);
                 }
             }
         }
