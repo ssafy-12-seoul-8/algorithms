44791408605a304bb9f48b1d9bb411331f719e53
sybang97 <sybang98@gmail.com>
2024-09-25 16:54:31 +0900

    [Gold V] Title: 평범한 배낭, Time: 108 ms, Memory: 13424 KB -SsafyHub

---
+++ 방승윤/백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] value = new int[K + 1];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int W = Integer.parseInt(st.nextToken());
+			int V = Integer.parseInt(st.nextToken());
+
+			if (W <= K) {
+				for (int j = K; j >= W; j--) {
+					value[j] = Math.max(value[j], value[j - W] + V);
+				}
+			}
+		}
+
+		System.out.println(value[K]);
+
+	}
+
+}
