48acee2327659dd4be07d56f786bf2d2612997a7
sybang97 <sybang98@gmail.com>
2024-09-25 22:41:26 +0900

    [Gold V] Title: 동전 2, Time: 80 ms, Memory: 11740 KB -SsafyHub

--- 방승윤/백준/Gold/2294. 동전 2/동전 2.java
+++ 방승윤/백준/Gold/2294. 동전 2/동전 2.java
@@ -14,31 +14,29 @@ public class Main {
 		int K = Integer.parseInt(st.nextToken());
 		boolean[] coin = new boolean[10001];
 		int[] cnt = new int[K + 1];
-		
-		Arrays.fill(cnt, Integer.MAX_VALUE);
+
+		Arrays.fill(cnt, 10001);
 		cnt[0] = 0;
-		
+
 		for (int i = 0; i < N; i++) {
 			int num = Integer.parseInt(br.readLine());
 			if (num <= K) {
 				coin[num] = true;
 			}
 		}
-		
+
 		for (int i = 1; i <= K; i++) {
 			if (coin[i]) {
 				for (int j = i; j <= K; j++) {
-					if (cnt[j - i] != Integer.MAX_VALUE) {
-						cnt[j] = Math.min(cnt[j - i] + 1, cnt[j]);
-					}
+					cnt[j] = Math.min(cnt[j - i] + 1, cnt[j]);
 				}
 			}
 		}
-		
-		if (cnt[K] == Integer.MAX_VALUE) {
+
+		if (cnt[K] == 10001) {
 			cnt[K] = -1;
 		}
-		
+
 		System.out.println(cnt[K]);
 
 	}
