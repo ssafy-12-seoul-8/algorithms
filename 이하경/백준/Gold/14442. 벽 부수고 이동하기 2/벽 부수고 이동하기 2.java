e593353648df9e11bbb63873687d161bf3775a13
puranium <63571947+puranium235@users.noreply.github.com>
2024-10-04 20:11:38 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1016 ms, Memory: 321356 KB -SsafyHub

--- 이하경/백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 이하경/백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -8,7 +9,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
@@ -32,9 +32,12 @@ public class Main {
         int[] dy = { 0, 0, 1, -1 };
 
         Queue<Point> q = new LinkedList<>();
-        boolean[][][] visit = new boolean[n][m][k + 1]; // 벽을 k개 부쉈을때 방문여부
+        int[][] visit = new int[n][m]; // 벽을 몇개 부쉈을때 방문했는지 -> 벽을 적게부수고 일찍 갈수있는곳이면 갈필요 x
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(visit[i], 11);
+        }
         q.add(new Point(0, 0, 1, 0));
-        visit[0][0][0] = true;
+        visit[0][0] = 0;
 
         while (!q.isEmpty()) {
             Point curr = q.poll();
@@ -49,16 +52,11 @@ public class Main {
                         return;
                     }
 
-                    if (map[nx][ny] == 0) { // 현재 부순개수에서 간적 없는곳
-                        if (!visit[nx][ny][curr.destroy]) {
-                            visit[nx][ny][curr.destroy] = true;
-                            q.add(new Point(nx, ny, curr.dist + 1, curr.destroy));
-                        }
-                    }
-                    else if(curr.destroy>=k)continue;
-                    else if (!visit[nx][ny][curr.destroy+1]) {
-                        visit[nx][ny][curr.destroy + 1] = true;
-                        q.add(new Point(nx, ny, curr.dist + 1, curr.destroy + 1));
+                    int nextDest = curr.destroy + map[nx][ny];
+
+                    if (nextDest <= k && visit[nx][ny] > nextDest) { // 이번에 넣을 값보다 큰 경우만 갱신
+                        visit[nx][ny] = nextDest;
+                        q.add(new Point(nx, ny, curr.dist + 1, nextDest));
                     }
                 }
             }
