9c39507f262eec8e0950e2d8c8f9cce50df816df
sybang97 <sybang98@gmail.com>
2024-10-04 16:10:00 +0900

    [Silver IV] Title: 제로, Time: 172 ms, Memory: 21632 KB -SsafyHub

---
+++ 방승윤/백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int K = Integer.parseInt(br.readLine());
+		Stack<Integer> stack = new Stack<>();
+		
+		for (int i = 0; i < K; i++) {
+			int num = Integer.parseInt(br.readLine());
+			
+			if (num == 0) {
+				stack.pop();
+				continue;
+			} else {
+				stack.push(num);
+			}
+		}
+		
+		int sum = 0;
+		
+		while (!stack.isEmpty()) {
+			sum += stack.pop();
+		}
+		
+		System.out.println(sum);
+		
+	}
+
+}
