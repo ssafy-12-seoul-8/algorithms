425053ae2f0c1b669b793bd262e040ad8a2f901f
puranium <63571947+puranium235@users.noreply.github.com>
2024-10-06 20:05:59 +0900

    [Gold V] Title: 퇴사 2, Time: 608 ms, Memory: 308208 KB -SsafyHub

--- 이하경/백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 이하경/백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,44 +8,25 @@ public class Main {
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        Counsel[] works = new Counsel[n];
+        int[] needDay = new int[n];
+        int[] income = new int[n];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int needDay = Integer.parseInt(st.nextToken());
-            int income = Integer.parseInt(st.nextToken());
-
-            works[i] = new Counsel(i, i + needDay, income); // startDay 미포함 endDay 포함
+            needDay[i] = Integer.parseInt(st.nextToken());
+            income[i] = Integer.parseInt(st.nextToken());
         }
-
-        Arrays.sort(works, Comparator.comparingInt(o -> o.endDay)); // 끝나는 날 오름차순 정렬
 
         // 모든 상담을 다 한다면 1000*1,500,000 < int
-        int[] dp = new int[n + 1]; // i일에 얻을 수 있는 최대수익
-        int lastCheck = 0;
-        for (Counsel c : works) {
-            if (c.endDay > n) {
-                break;
+        int[] dp = new int[n + 1]; // i일 이후에 얻을 수 있는 최대수익
+        for (int i = n - 1; i >= 0; i--) {
+            if (i + needDay[i] > n) {
+                dp[i] = dp[i + 1];
+                continue;
             }
-
-            for (int i = lastCheck + 1; i <= c.endDay; i++) {
-                dp[i] = dp[lastCheck]; // 마지막으로 구한 날짜 다음을 같은값으로 채워준다
-            }
-
-            dp[c.endDay] = Math.max(dp[c.endDay], dp[c.startDay] + c.income);
-            lastCheck = c.endDay;
+            dp[i] = Math.max(dp[i + 1], dp[i + needDay[i]] + income[i]);
         }
 
-        System.out.println(dp[lastCheck]);
-    }
-
-    static class Counsel {
-        int startDay, endDay, income;
-
-        public Counsel(int startDay, int endDay, int income) {
-            this.startDay = startDay;
-            this.endDay = endDay;
-            this.income = income;
-        }
+        System.out.println(dp[0]);
     }
 }
