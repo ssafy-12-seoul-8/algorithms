41cd5a9c9c5d9e481c4a9554a648cb0f9bbca100
sybang97 <sybang98@gmail.com>
2024-10-09 20:00:36 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 60 ms, Memory: 11448 KB -SsafyHub

---
+++ 방승윤/백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+    public static void main(String[] args) throws Exception {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    	switch (br.readLine()) {
+    	case "NLCS":
+    		System.out.println("North London Collegiate School");
+    		break;
+    	case "BHA":
+    		System.out.println("Branksome Hall Asia");
+    		break;
+    	case "KIS":
+    		System.out.println("Korea International School");
+    		break;
+    	case "SJA":
+    		System.out.println("St. Johnsbury Academy");
+    		break;
+		}
+    	
+    }
+    
+}
