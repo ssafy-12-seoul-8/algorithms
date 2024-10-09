221fe6ee3138295affc458e937c1644de261d54b
sybang97 <sybang98@gmail.com>
2024-10-09 19:52:23 +0900

    [Bronze V] Title: 홍익대학교, Time: 64 ms, Memory: 11492 KB -SsafyHub

---
+++ 방승윤/백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+    public static void main(String[] args) throws Exception {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	System.out.print(Integer.parseInt(br.readLine()) - 1946);
+    	
+    }
+}
