7cc5274ad2277acd8d82996da1f59b2afc6293f0
puranium <63571947+puranium235@users.noreply.github.com>
2024-10-11 12:13:04 +0900

    [Gold III] Title: 치즈, Time: 100 ms, Memory: 12800 KB -SsafyHub

--- 이하경/백준/Gold/2638. 치즈/치즈.java
+++ 이하경/백준/Gold/2638. 치즈/치즈.java
@@ -1,9 +1,12 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int n, m;
+	static Deque<Point> q;
 	static boolean[][] visit;
 	static int[][] map; // 0은 내부, 1은 치즈
 	static int[] dx = { 1, -1, 0, 0 };
@@ -33,62 +36,82 @@ public class Main {
 	}
 
 	static void solve() {
-		int time = 0;
-		while (cheeseCnt > 0) {
-			time++;
-			visit = new boolean[n][m];
+		q = new ArrayDeque<>();
+		visit = new boolean[n][m];
 
-			for (int i = 0; i < n; i++) {
-				if (map[i][0] == 0 && !visit[i][0]) {
-					checkTouch(i, 0);
-				}
-				if (map[i][m - 1] == 0 && !visit[i][m - 1]) {
-					checkTouch(i, m - 1);
-				}
+		for (int i = 0; i < n; i++) {
+			if (map[i][0] == 0) {
+				visit[0][0] = true;
+				q.addFirst(new Point(i, 0, 0));
 			}
-
-			for (int i = 1; i < m; i++) {
-				if (map[0][i] == 0 && !visit[0][i]) {
-					checkTouch(0, i);
-				}
-				if (map[n - 1][i] == 0 && !visit[n - 1][i]) {
-					checkTouch(n - 1, i);
-				}
+			if (map[i][m - 1] == 0) {
+				visit[i][m - 1] = true;
+				q.addFirst(new Point(i, m - 1, 0));
 			}
 		}
-		System.out.println(time);
+
+		for (int i = 1; i < m; i++) {
+			if (map[0][i] == 0) {
+				visit[0][i] = true;
+				q.addFirst(new Point(0, i, 0));
+			}
+			if (map[n - 1][i] == 0) {
+				visit[n - 1][i] = true;
+				q.addFirst(new Point(n - 1, i, 0));
+			}
+		}
+
+		System.out.println(bfs());
 	}
 
 
-	static void checkTouch(int x, int y) {
-		visit[x][y] = true;
+	static int bfs() {
+		int maxTime = 0;
 
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
+		while (!q.isEmpty()) {
+			Point curr = q.pollFirst();
 
-			if (!inRange(nx, ny)) {
-				continue;
-			}
+			for (int d = 0; d < 4; d++) {
+				int nx = curr.x + dx[d];
+				int ny = curr.y + dy[d];
 
-			if (map[nx][ny] == 0 && !visit[nx][ny]) {
-				checkTouch(nx, ny);
-				continue;
-			}
+				if (!inRange(nx, ny)) {
+					continue;
+				}
 
-			if (map[nx][ny] == 1 && !visit[nx][ny]) {
-				visit[nx][ny] = true;
-				continue;
-			}
-
-			if (map[nx][ny] == 1 && visit[nx][ny]) {
-				map[nx][ny] = 0;
-				cheeseCnt--;
+				if (map[nx][ny] == 0 && !visit[nx][ny]) {
+					visit[nx][ny] = true;
+					q.addFirst(new Point(nx, ny, curr.time));
+					continue;
+				}
+				
+				if (map[nx][ny] == 1 && !visit[nx][ny]) {
+					visit[nx][ny] = true;
+					continue;
+				}
+				
+				if (map[nx][ny] == 1 && visit[nx][ny]) {
+					map[nx][ny] = 0;
+					maxTime = curr.time + 1;
+					q.addLast(new Point(nx, ny, curr.time + 1));
+				}
 			}
 		}
+
+		return maxTime;
 	}
 
 	static boolean inRange(int nx, int ny) {
 		return nx >= 0 && nx < n && ny >= 0 && ny < m;
+	}
+
+	static class Point {
+		int x, y, time;
+
+		public Point(int x, int y, int time) {
+			this.x = x;
+			this.y = y;
+			this.time = time;
+		}
 	}
 }
