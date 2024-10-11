f366700e23d4b60b871e459f7cffb2d8e903de70
sybang97 <sybang98@gmail.com>
2024-10-11 17:19:25 +0900

    [Gold V] Title: A와 B, Time: 72 ms, Memory: 12008 KB -SsafyHub

---
+++ 방승윤/백준/Gold/12904. A와 B/A와 B.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] S = br.readLine().split("");
+		String[] T = br.readLine().split("");
+		boolean dir = true;
+		int L = 0;
+		int R = T.length - 1;
+		int ans = 1;
+		
+		while (S.length != R + 1 - L) {
+			if (dir) {
+				switch (T[R]) {
+				case "A":
+					R--;
+					break;
+				case "B":
+					R--;
+					dir = false;
+					break;
+				}
+			} else {
+				switch (T[L]) {
+				case "A":
+					L++;
+					break;
+				case "B":
+					L++;
+					dir = true;
+					break;
+				}
+			}
+		}
+		
+		if (dir) {
+			for (int i = 0; i < S.length; i++) {
+				if (!S[i].equals(T[L + i])) {
+					ans = 0;
+					break;
+				}
+			}
+		} else {
+			for (int i = 0; i < S.length; i++) {
+				if (!S[i].equals(T[R - i])) {
+					ans = 0;
+					break;
+				}
+			}
+		}
+		
+		System.out.println(ans);
+
+	}
+
+}
