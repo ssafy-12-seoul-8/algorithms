f0cd2d7178797c220018147a8ab86095bfcc0a5f
puranium <63571947+puranium235@users.noreply.github.com>
2024-10-11 09:42:04 +0900

    [Silver IV] Title: 비트가 넘쳐흘러, Time: 144 ms, Memory: 22420 KB, Score: 100 point -SsafyHub

--- 이하경/백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.java
+++ 이하경/백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.java
@@ -2,13 +2,22 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 // K&((~K)+1) = 가장 작은 자릿수의 1만 찍혀있음
-// K-K&((~K)+1) = 가장 작은 자릿수의 1을 삭제한다
+// K-K&((~K)+1) = 가장 작은 자릿수의 1을 삭제한다 -> 1의 개수가 정답
+
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		br.readLine();
 
-		int k = Integer.parseInt(br.readLine(), 2);
-		System.out.println(Integer.bitCount(k));
+		char[] k = br.readLine().toCharArray();
+		int cnt = 0;
+
+		for (char c : k) {
+			if (c == '1') {
+				cnt++;
+			}
+		}
+
+		System.out.println(cnt);
 	}
 }
