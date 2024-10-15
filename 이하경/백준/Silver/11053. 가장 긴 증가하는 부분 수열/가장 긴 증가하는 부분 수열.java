37bdf9ddfea4f63ac91981a7dd84f766c41e87e9
puranium <63571947+puranium235@users.noreply.github.com>
2024-10-15 10:28:21 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 84 ms, Memory: 11916 KB -SsafyHub

--- 이하경/백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 이하경/백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,6 +1,7 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,17 +12,24 @@ public class Main {
 
 		int n = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
 
-		int[] dp = new int[1001]; // i보다 작은 수로로 끝나는 LIS 최대길이
-		while (n-- > 0) {
-			int a = Integer.parseInt(st.nextToken());
-			int tmp = dp[a - 1] + 1;
-			for (int i = a; i <= 1000; i++) {
-				dp[i] = Math.max(dp[i], tmp);
+		int[] dp = new int[n]; // i번째 숫자로 끝나는 LIS 길이
+		Arrays.fill(dp, 1);
+		int max = 1;
+
+		for (int i = 1; i < n; i++) { // dp[i]를 채울거야
+			for (int j = 0; j < i; j++) { // 근데 앞에 숫자들을 다 확인하려고
+				if (arr[i] > arr[j]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
 			}
+			max = Math.max(max, dp[i]);
 		}
 		
-		System.out.println(dp[1000]);
-
+		System.out.println(max);
 	}
 }
