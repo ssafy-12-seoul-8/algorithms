5b7c7f1dc5fc25169e317d8178b4068f76198e53
puranium <63571947+puranium235@users.noreply.github.com>
2024-10-15 10:56:12 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 68 ms, Memory: 11764 KB -SsafyHub

--- 이하경/백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 이하경/백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,35 +1,43 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
 		int n = Integer.parseInt(br.readLine());
+
+		int[] lis = new int[n + 1]; // 길이가 i인 부분수열 중 마지막 값의 최소
+		int end = 0;
+
 		st = new StringTokenizer(br.readLine());
-		int[] arr = new int[n];
 		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-
-		int[] dp = new int[n]; // i번째 숫자로 끝나는 LIS 길이
-		Arrays.fill(dp, 1);
-		int max = 1;
-
-		for (int i = 1; i < n; i++) { // dp[i]를 채울거야
-			for (int j = 0; j < i; j++) { // 근데 앞에 숫자들을 다 확인하려고
-				if (arr[i] > arr[j]) {
-					dp[i] = Math.max(dp[i], dp[j] + 1);
-				}
+			int key = Integer.parseInt(st.nextToken());
+			if (key > lis[end]) {
+				lis[++end] = key;
+				continue;
 			}
-			max = Math.max(max, dp[i]);
+			int idx = binarySearch(lis, 0, end, key);
+			lis[idx] = key;
 		}
 		
-		System.out.println(max);
+		System.out.println(end);
+
+	}
+
+	// a가 들어갈 위치 = a보다 크거나 같은 숫자가 처음으로 나오는 위치
+	static int binarySearch(int[] arr, int start, int end, int key) {
+		while (start < end) {
+			int mid = (start + end) / 2;
+			if (key <= arr[mid]) { // 크거나 같은 숫자가 나오면 더 큰쪽은 볼필요x
+				end = mid;
+				continue;
+			}
+			start = mid + 1;
+		}
+		return end;
 	}
 }
