b039efe82d9d3f8e114e227855f47a87a95109df
sybang97 <sybang98@gmail.com>
2024-10-16 00:21:53 +0900

    [Silver II] Title: 최소 힙, Time: 240 ms, Memory: 24700 KB -SsafyHub

---
+++ 방승윤/백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(br.readLine());
+			
+			if (num == 0) {
+				if (pq.isEmpty()) {
+					sb.append(0).append("\n");
+				} else {
+					sb.append(pq.poll()).append("\n");
+				}
+			} else {
+				pq.offer(num);
+			}
+		}
+		
+		System.out.println(sb);
+
+	}
+
+}
