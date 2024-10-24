d5bd59630e4476aafa3fbeca6cf7761b22589315
Hangyu Sang / 상 한규 <68945250+LittleWaterDrops@users.noreply.github.com>
2024-10-24 19:56:03 +0900

    [Silver IV] Title: 수 찾기, Time: 1596 ms, Memory: 74032 KB -SsafyHub

---
+++ 상한규/백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+/*
+수 찾기 문제의 경우 N의 개수가 10만이기 때문에 nlogn이 가능한 이분 탐색을 쓴다. 카운팅 배열을 쓰면 빠르게 가능하지만,
+정수의 범위가 2의 31제곱 즉 2억이기 때문에 채택하지 않는다.
+해쉬셋의 경우는 가능하나 이분탐색보다는 느리다.
+*/ 
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] A;
+    static int[] checks;
+    
+    public static void main(String[] args) throws IOException {
+      // BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        A = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+        
+        M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        checks = new int[M];
+
+        for(int i = 0; i < M; i++) {
+            checks[i] = Integer.parseInt(st.nextToken());
+        }
+        
+
+        for(int value : checks) {
+            boolean isExist = binarySearch(value);
+            
+            System.out.println(isExist ? 1 : 0);
+        }
+    }
+    
+    static boolean binarySearch(int target) {
+        int start = 0;
+        int end = N - 1;
+        boolean isExist = false;
+
+        while(start <= end) {
+            int mid = (end + start) / 2;
+            if(A[mid] == target) {
+                isExist = true;
+
+                break;
+            } else {
+                if(target > A[mid]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return isExist;
+    }
+}
