b510c9c6bc07df5cbe5d1721cfcc61711aa430ac
sybang97 <sybang98@gmail.com>
2024-10-25 22:23:32 +0900

    [Silver II] Title: 좌표 압축, Time: 1388 ms, Memory: 229256 KB -SsafyHub

--- 방승윤/백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 방승윤/백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,25 +9,33 @@ public class Main {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		HashSet<Integer> set = new HashSet<>();
 
 		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N];
+		int[] sortedArr = new int[N];
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
-			set.add(arr[i]);
+			sortedArr[i] = arr[i];
 		}
 		
-		Integer[] sortedArr = new Integer[set.size()];
-		set.toArray(sortedArr);
 		Arrays.sort(sortedArr);
+		
+		int idx = 0;
+		int prev = sortedArr[0];
+		
+		for (int i = 1; i < N; i++) {
+			if (sortedArr[i] != prev) {
+				sortedArr[++idx] = sortedArr[i];
+				prev = sortedArr[i];
+			}
+		}
 		
 		for (int i = 0; i < N; i++) {
 			int L = 0;
-			int R = sortedArr.length - 1;
+			int R = idx;
 			
 			while (L <= R) {
 				int mid = (L + R) / 2;
