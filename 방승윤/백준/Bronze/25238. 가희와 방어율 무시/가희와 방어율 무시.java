99938f617c4ff5583d4c0852e4668c833905385a
sybang97 <sybang98@gmail.com>
2024-11-05 00:06:33 +0900

    [Bronze IV] Title: 가희와 방어율 무시, Time: 68 ms, Memory: 11480 KB -SsafyHub

---
+++ 방승윤/백준/Bronze/25238. 가희와 방어율 무시/가희와 방어율 무시.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		System.out.println(a * (100 - b) >= 10000 ? 0 : 1);
+		
+	}
+	
+}
