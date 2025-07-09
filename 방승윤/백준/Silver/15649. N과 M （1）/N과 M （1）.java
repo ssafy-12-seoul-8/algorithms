398aeb6e601e987c04d08cce47c9775b97dc5459
sybang97 <sybang98@gmail.com>
2025-07-09 21:02:17 +0900

    [Silver III] Title: N과 M (1), Time: 252 ms, Memory: 25728 KB -SsafyHub

---
+++ 방승윤/백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static Stack<Integer> stack;
+    static StringBuilder sb = new StringBuilder();;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        stack = new Stack<>();
+
+        recur(0);
+
+        System.out.println(sb);
+    }
+
+    public static void recur(int level) {
+        if (level == M) {
+            for (int num : stack) {
+                sb.append(num).append(" ");
+            }
+
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!stack.contains(i)) {
+                stack.add(i);
+                recur(level + 1);
+                stack.pop();
+            }
+        }
+    }
+}
