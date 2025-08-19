ffae38a068d618e38eed753b4c3120169498fbf3
sybang97 <sybang98@gmail.com>
2025-08-19 17:43:44 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 456 ms, Memory: 56484 KB -SsafyHub

--- 방승윤/백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 방승윤/백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,34 +1,31 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] dp = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
 
-	public static void main(String[] args) throws Exception {
+        for (int i = 1; i <= N; i++) {
+            dp[i] = dp[i - 1] + Integer.parseInt(st.nextToken());
+        }
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int[] dp = new int[N + 1];
-		
-		st = new StringTokenizer(br.readLine());
-		
-		for (int i = 1; i <= N; i++) {
-			dp[i] = dp[i - 1] + Integer.parseInt(st.nextToken());
-		}
-		
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int L = Integer.parseInt(st.nextToken());
-			int R = Integer.parseInt(st.nextToken());
-			sb.append(dp[R] - dp[L - 1]).append("\n");
-		}
-		
-		System.out.println(sb);
-		
-	}
+        for (int tc = 0; tc < M; tc++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            sb.append(dp[j] - dp[i - 1]).append("\n");
+        }
 
+        System.out.println(sb);
+    }
 }
