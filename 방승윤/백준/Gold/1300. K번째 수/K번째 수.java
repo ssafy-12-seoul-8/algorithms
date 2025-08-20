e5d39c845051442a55f4767b3ba8739c059cc64a
sybang97 <sybang98@gmail.com>
2025-08-21 01:52:01 +0900

    [Gold I] Title: K번째 수, Time: 88 ms, Memory: 11580 KB -SsafyHub

--- 방승윤/백준/Gold/1300. K번째 수/K번째 수.java
+++ 방승윤/백준/Gold/1300. K번째 수/K번째 수.java
@@ -1,43 +1,36 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
+        int L = 1;
+        int R = k;
 
-	public static void main(String[] args) throws Exception {
+        while (L <= R) {
+            int mid = (L + R) / 2;
+            int cnt = 0;
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            for (int i = 1; i <= N; i++) {
+                if (i > mid) {
+                    break;
+                }
 
-		int N = Integer.parseInt(br.readLine());
-		int k = Integer.parseInt(br.readLine());
+                cnt += Math.min(N, mid / i);
+            }
 
-		int L = 1;
-		int R = k;
-		int ans = 0;
+            if (cnt >= k) {
+                R = mid - 1;
+            }
 
-		while (L <= R) {
-			int mid = (L + R) / 2;
-			int cnt = 0;
+            if (cnt < k) {
+                L = mid + 1;
+            }
+        }
 
-			for (int i = 1; i <= N; i++) {
-				if (i > mid) {
-					break;
-				} else if (mid >= (long) i * N) {
-					cnt += N;
-				} else {
-					cnt += mid / i;
-				}
-			}
-
-			if (cnt >= k) {
-				R = mid - 1;
-				ans = mid;
-			} else if (cnt < k) {
-				L = mid + 1;
-			}
-		}
-
-		System.out.println(ans);
-
-	}
-
+        System.out.println(L);
+    }
 }
