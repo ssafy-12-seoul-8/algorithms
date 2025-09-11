042156dfac3a034c36dc50fe1cf61de6167de8c9
puranium <63571947+puranium235@users.noreply.github.com>
2025-09-11 16:51:22 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 68 ms, Memory: 12508 KB -SsafyHub

--- 이하경/백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 이하경/백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -1,8 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+
+// 1. i가 n보다 작으면 -1로만 이동
+// 2. i의 최댓값은 i-1에서 +1 이동
+// 3. n이 짝수면 2배
+// 4. n이 홀수면 2배 후 +-1
+// 5. -1이동의 경우는 짝수는 절대 안생김, 홀수는 +1은 2에서 했으므로 -1만 고려해주자
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -13,46 +18,49 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
+        if (k <= n) {
+            sb.append(n - k).append("\n").append(1);
+            System.out.println(sb);
+            return;
+        }
+
         int[] dist = new int[100_001];
         int[] count = new int[100_001];
-        dist[n] = 1;
+        
         count[n] = 1;
 
-        Queue<Integer> q = new LinkedList<>();
-        q.add(n);
+        for (int i = 0; i < n; i++) {
+            dist[i] = n - i;
+            count[i] = 1;
+        }
 
-        int currDist = 1;
-        while (!q.isEmpty()) {
-            int curr = q.poll();
+        for (int i = n + 1; i <= k; i++) {
+            dist[i] = dist[i - 1] + 1;
+            count[i] = count[i - 1];
 
-            if (dist[curr] > currDist) {
-                currDist = dist[curr];
-                if (dist[k] != 0) {
-                    break;
+            if (i % 2 == 0) {
+                if (dist[i / 2] + 1 < dist[i]) {
+                    dist[i] = dist[i / 2] + 1;
+                    count[i] = count[i / 2];
+                    continue;
                 }
+                if (dist[i / 2] + 1 == dist[i]) {
+                    count[i] += count[i / 2];
+                }
+                continue;
             }
 
-            for (int next : move(curr)) {
-                if (next >= 0 && next <= 100_000) {
-                    if (dist[next] == 0) {
-                        dist[next] = dist[curr] + 1;
-                        count[next] += count[curr];
-                        q.offer(next);
-                        continue;
-                    }
-
-                    if (dist[next] == dist[curr] + 1) {
-                        count[next] += count[curr];
-                    }
-                }
+            if (dist[i / 2 + 1] + 2 < dist[i]) {
+                dist[i] = dist[i / 2 + 1] + 2;
+                count[i] = count[i / 2 + 1];
+                continue;
+            }
+            if (dist[i / 2 + 1] + 2 == dist[i]) {
+                count[i] += count[i / 2 + 1];
             }
         }
 
-        sb.append(dist[k] - 1).append("\n").append(count[k]);
+        sb.append(dist[k]).append("\n").append(count[k]);
         System.out.println(sb);
-    }
-
-    public static int[] move(int n) {
-        return new int[]{n + 1, n - 1, n * 2};
     }
 }
