c882ac4d4d8f49a4c3b5f67815d5a406415542ca
sybang97 <sybang98@gmail.com>
2025-09-12 16:42:38 +0900

    [Silver V] Title: 피보나치 수 4, Time: 112 ms, Memory: 18128 KB -SsafyHub

---
+++ 방승윤/백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        if (n == 0) {
+            System.out.println(0);
+            return;
+        }
+        
+        BigInteger[] dp = new BigInteger[n + 1];
+        dp[0] = BigInteger.ZERO;
+        dp[1] = BigInteger.ONE;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 2].add(dp[i - 1]);
+        }
+
+        System.out.println(dp[n]);
+    }
+}
