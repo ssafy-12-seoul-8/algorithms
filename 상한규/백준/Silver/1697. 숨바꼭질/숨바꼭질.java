dfcb6fd6561610dac96c9746e804ab4d4dd02eed
Hangyu Sang / 상 한규 <68945250+LittleWaterDrops@users.noreply.github.com>
2025-09-15 17:55:09 +0900

    [Silver I] Title: 숨바꼭질, Time: 184 ms, Memory: 75972 KB -SsafyHub

---
+++ 상한규/백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  static public void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int K = Integer.parseInt(st.nextToken());
+    int result = Integer.MAX_VALUE;
+    boolean[] visited = new boolean[100001];
+    Queue<int[]> queue = new LinkedList<>();
+    
+    queue.add(new int[] {N, 0});
+
+    while(!queue.isEmpty()) {
+      int[] current = queue.poll();
+      visited[current[0]] = true;
+
+      if(current[0] == K) {
+        result = result < current[1] ? result : current[1];
+        
+        break;
+      }
+
+      if(current[0] > 0 && visited[current[0] - 1] == false) {
+        queue.add(new int[] {current[0] - 1, current[1] + 1});
+      }
+
+      if(current[0] < 100000 && visited[current[0] + 1] == false) {
+        queue.add(new int[] {current[0] + 1, current[1] + 1});
+      }
+
+      if(current[0] * 2 <= 100000 && current[0] != 0 && visited[current[0] * 2] == false) {
+        queue.add(new int[] {current[0] * 2, current[1] + 1});
+      }
+    }
+
+    System.out.println(result);
+  }
+}
