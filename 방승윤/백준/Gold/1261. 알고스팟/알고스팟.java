51aed70c4e57e715a18a0afbc63254b7a676048f
sybang97 <sybang98@gmail.com>
2025-09-23 14:02:51 +0900

    [Gold IV] Title: 알고스팟, Time: 88 ms, Memory: 12340 KB -SsafyHub

---
+++ 방승윤/백준/Gold/1261. 알고스팟/알고스팟.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    public static class Node implements Comparable<Node> {
+        int x;
+        int y;
+        int d;
+
+        Node(int x, int y, int d) {
+            this.x = x;
+            this.y = y;
+            this.d = d;
+        }
+
+        @Override
+        public int compareTo(Node node) {
+            return this.d - node.d;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N][M];
+        int[][] dist = new int[N][M];
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            Arrays.fill(dist[i], Integer.MAX_VALUE / 10);
+
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(0, 0, 0));
+        dist[0][0] = 0;
+
+        while (!pq.isEmpty()) {
+            Node curr = pq.poll();
+            int x = curr.x;
+            int y = curr.y;
+            int d = curr.d;
+
+            if (d > dist[x][y]) {
+                continue;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 0 || nx >= N || ny <0 || ny >= M) {
+                    continue;
+                }
+
+                int nd = d + arr[nx][ny];
+
+                if (nd < dist[nx][ny]) {
+                    dist[nx][ny] = nd;
+                    pq.add(new Node(nx, ny, nd));
+                }
+            }
+        }
+
+        System.out.println(dist[N - 1][M - 1]);
+    }
+}
