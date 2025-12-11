6f2888a8a3d5cf098760c1824c0f0fa4541768e0
sybang97 <sybang98@gmail.com>
2025-12-11 23:16:40 +0900

    [level 3] Title: 카운트 다운, Time: 58.73 ms, Memory: 81.4 MB -SsafyHub

---
+++ 방승윤/프로그래머스/3/131129. 카운트 다운/카운트 다운.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int target) {
+        int[][] dp = new int[target + 1][2];
+        
+        for (int i = 1; i <= target; i++) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+        
+        for (int i = 1; i <= target; i++) {
+            for (int j = 1; j <= 20; j++) {
+                for (int k = 1; k <= 3; k++) {
+                    if (i - j * k >= 0) {
+                        if (dp[i - j * k][0] + 1 == dp[i][0]) {
+                            if (k == 1){
+                                dp[i][1] = Math.max(dp[i][1], dp[i - j][1] + 1);
+                            } else {
+                                dp[i][1] = Math.max(dp[i][1], dp[i - j * k][1]);
+                            }
+                        } else if (dp[i - j * k][0] + 1 < dp[i][0]) {
+                            dp[i][0] = dp[i - j * k][0] + 1;
+                            dp[i][1] = k == 1 ? dp[i - j][1] + 1 : dp[i - j * k][1];
+                        }
+                    }
+                }
+            }
+            
+            if (i - 50 >= 0) {
+                if (dp[i - 50][0] + 1 == dp[i][0]) {
+                    dp[i][1] = Math.max(dp[i][1], dp[i - 50][1] + 1);
+                } else if (dp[i - 50][0] + 1 < dp[i][0]) {
+                    dp[i][0] = dp[i - 50][0] + 1;
+                    dp[i][1] = dp[i - 50][1] + 1;
+                }
+            }
+        }
+        
+        return dp[target];
+    }
+}
