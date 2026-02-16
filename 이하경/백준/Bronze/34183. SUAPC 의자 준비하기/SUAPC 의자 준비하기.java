6875570b45e65ef204779ec62aa478de4da58be3
puranium <63571947+puranium235@users.noreply.github.com>
2026-02-16 20:03:57 +0900

    [Bronze IV] Title: SUAPC 의자 준비하기, Time: 60 ms, Memory: 11496 KB -SsafyHub

---
+++ 이하경/백준/Bronze/34183. SUAPC 의자 준비하기/SUAPC 의자 준비하기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int need = n * 3;
+
+        if (m >= need) {
+            System.out.println(0);
+            return;
+        }
+
+        System.out.println(a * (need - m) + b);
+    }
+}
