8991bb92426818bafbc44ba65a35630d3838c0c8
puranium <63571947+puranium235@users.noreply.github.com>
2026-02-25 15:29:06 +0900

    [Gold III] Title: LCS 2, Time: 108 ms, Memory: 16944 KB -SsafyHub

--- 이하경/백준/Gold/9252. LCS 2/LCS 2.java
+++ 이하경/백준/Gold/9252. LCS 2/LCS 2.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -9,62 +8,36 @@ public class Main {
 
         String s1 = br.readLine();
         String s2 = br.readLine();
-        int[] lcs = new int[1000];
-        Node[] tailNodes = new Node[1000];
-        int maxIdx = 0;
 
-        for (int i = 0; i < s1.length(); i++) {
-            for (int j = s2.length() - 1; j >= 0; j--) {
-                if (s1.charAt(i) == s2.charAt(j)) {
-                    int idx = binarySearch(lcs, 0, maxIdx, j);
-                    if (idx == maxIdx) {
-                        maxIdx++;
-                    }
-                    lcs[idx] = j;
-                    if (idx == 0) {
-                        tailNodes[idx] = new Node(j, null);
-                    } else {
-                        tailNodes[idx] = new Node(j, tailNodes[idx - 1]);
-                    }
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        for (int i = 1; i < s1.length() + 1; i++) {
+            for (int j = 1; j < s2.length() + 1; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    continue;
                 }
+                dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
             }
         }
 
-        sb.append(maxIdx).append("\n");
-        if (maxIdx != 0) {
-            Node currNode = tailNodes[maxIdx - 1];
-            ArrayList<Character> list = new ArrayList<>();
-            while (currNode != null) {
-                list.add(s2.charAt(currNode.pos));
-                currNode = currNode.parent;
-            }
-            for (int i = list.size() - 1; i >= 0; i--) {
-                sb.append(list.get(i));
-            }
-        }
-
+        sb.append(dp[s1.length()][s2.length()]).append("\n");
+        print(s1.length(), s2.length(), s1, s2, sb, dp);
         System.out.println(sb);
     }
 
-    static int binarySearch(int[] arr, int start, int end, int target) {
-        while (start < end) {
-            int mid = (start + end) / 2;
-            if (arr[mid] < target) {
-                start = mid + 1;
-                continue;
-            }
-            end = mid;
+    public static void print(int i, int j, String s1, String s2, StringBuilder sb, int[][] dp) {
+        if (dp[i][j] == 0) {
+            return;
         }
-        return end;
-    }
-
-    private static class Node {
-        int pos;
-        Node parent;
-
-        public Node(int pos, Node parent) {
-            this.pos = pos;
-            this.parent = parent;
+        if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+            print(i - 1, j - 1, s1, s2, sb, dp);
+            sb.append(s1.charAt(i - 1));
+            return;
+        }
+        if (dp[i - 1][j] >= dp[i][j - 1]) {
+            print(i - 1, j, s1, s2, sb, dp);
+        } else {
+            print(i, j - 1, s1, s2, sb, dp);
         }
     }
 }
