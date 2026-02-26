0fbcdc79b46e9a291d757cec4ad2a17730371f96
Hangyu Sang / 상 한규 <68945250+LittleWaterDrops@users.noreply.github.com>
2026-02-26 23:39:26 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 612 ms, Memory: 59300 KB -SsafyHub

---
+++ 상한규/백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] points = new int[N][2];
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            points[i][0] = Integer.parseInt(st.nextToken());
+            points[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(points, (p1, p2) -> {
+            if (p1[1] == p2[1]) {
+                return p1[0] - p2[0];
+            }
+            return p1[1] - p2[1];
+        });
+
+        for (int i = 0; i < N; i++) {
+            sb.append(points[i][0]).append(" ").append(points[i][1]).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
