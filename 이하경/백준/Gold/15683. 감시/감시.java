9e200e0d26602be862b136d9e9f400ee017bc2e1
puranium <63571947+puranium235@users.noreply.github.com>
2026-02-27 13:34:07 +0900

    [Gold III] Title: 감시, Time: 76 ms, Memory: 11872 KB -SsafyHub

--- 이하경/백준/Gold/15683. 감시/감시.java
+++ 이하경/백준/Gold/15683. 감시/감시.java
@@ -8,7 +8,7 @@ public class Main {
     static int n;
     static int m;
     static int[][] room;
-    static ArrayList<int[]> cctv;
+    static ArrayList<long[]> view;
     static int min;
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
@@ -20,211 +20,93 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         room = new int[n][m];
-        cctv = new ArrayList<>();
         min = n * m;
+        view = new ArrayList<>();
+        long bit = 0;
 
-        int count = 0;
+        ArrayList<int[]> cctv = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
                 room[i][j] = Integer.parseInt(st.nextToken());
-                if (room[i][j] >= 1 && room[i][j] <= 5) {
-                    cctv.add(new int[]{i, j, room[i][j]});
+                if (room[i][j] == 6) {
+                    bit |= pointToBit(i, j);
+                    continue;
                 }
-                if (room[i][j] == 0) {
-                    count++;
+                if (room[i][j] >= 1) {
+                    cctv.add(new int[]{ i, j });
                 }
             }
         }
 
-        bt(0, count);
+        for (int[] c:cctv) {
+            view.add(getView(c[0], c[1], room[c[0]][c[1]]));
+        }
+
+        bt(0, bit);
         System.out.println(min);
     }
 
-    public static void bt(int idx, int count) {
-        if (idx == cctv.size()) {
-            min = Math.min(min, count);
-            return;
-        }
-
-        if (count == 0) {
+    public static void bt(int idx, long bit) {
+        if (Long.bitCount(bit) == n * m) {
             min = 0;
             return;
         }
 
-        int[] c = cctv.get(idx);
-
-        ArrayList<int[]> tmp = new ArrayList<>();
-        if (c[2] == 1) {
-            for (int d = 0; d < 4; d++) {
-                int nx = c[0] + dx[d];
-                int ny = c[1] + dy[d];
-                while (nx >= 0 && nx < n && ny >= 0 && ny < m && room[nx][ny] != 6) {
-                    if (room[nx][ny] == 0) {
-                        room[nx][ny] = -1;
-                        tmp.add(new int[]{nx, ny});
-                    }
-                    nx += dx[d];
-                    ny += dy[d];
-                }
-                bt(idx + 1, count - tmp.size());
-                for (int[] t : tmp) {
-                    room[t[0]][t[1]] = 0;
-                }
-                tmp.clear();
-            }
+        if (idx == view.size()) {
+            min = Math.min(min, n * m - Long.bitCount(bit));
             return;
         }
 
-        if (c[2] == 2) {
-            for (int d = 0; d < 2; d++) {
-                int nx = c[0] + dx[d];
-                int ny = c[1] + dy[d];
-                while (nx >= 0 && nx < n && ny >= 0 && ny < m && room[nx][ny] != 6) {
-                    if (room[nx][ny] == 0) {
-                        room[nx][ny] = -1;
-                        tmp.add(new int[]{nx, ny});
-                    }
-                    nx += dx[d];
-                    ny += dy[d];
-                }
-                nx = c[0] + dx[d + 2];
-                ny = c[1] + dy[d + 2];
-                while (nx >= 0 && nx < n && ny >= 0 && ny < m && room[nx][ny] != 6) {
-                    if (room[nx][ny] == 0) {
-                        room[nx][ny] = -1;
-                        tmp.add(new int[]{nx, ny});
-                    }
-                    nx += dx[d + 2];
-                    ny += dy[d + 2];
-                }
-                bt(idx + 1, count - tmp.size());
-                for (int[] t : tmp) {
-                    room[t[0]][t[1]] = 0;
-                }
-                tmp.clear();
-            }
-            return;
+        for (long viewBit:view.get(idx)) {
+            bt(idx + 1, bit | viewBit);
+        }
+    }
+
+    public static long[] getView(int x, int y, int type) {
+        long[] dirBits = new long[4];
+        for (int i = 0; i < 4; i++) {
+            dirBits[i] = getDirBit(x, y, i);
         }
 
-        if (c[2] == 3) {
-            for (int d = 0; d < 4; d++) {
-                int nx = c[0] + dx[d];
-                int ny = c[1] + dy[d];
-                while (nx >= 0 && nx < n && ny >= 0 && ny < m && room[nx][ny] != 6) {
-                    if (room[nx][ny] == 0) {
-                        room[nx][ny] = -1;
-                        tmp.add(new int[]{nx, ny});
-                    }
-                    nx += dx[d];
-                    ny += dy[d];
-                }
-                nx = c[0] + dx[(d + 1) % 4];
-                ny = c[1] + dy[(d + 1) % 4];
-                while (nx >= 0 && nx < n && ny >= 0 && ny < m && room[nx][ny] != 6) {
-                    if (room[nx][ny] == 0) {
-                        room[nx][ny] = -1;
-                        tmp.add(new int[]{nx, ny});
-                    }
-                    nx += dx[(d + 1) % 4];
-                    ny += dy[(d + 1) % 4];
-                }
-                bt(idx + 1, count - tmp.size());
-                for (int[] t : tmp) {
-                    room[t[0]][t[1]] = 0;
-                }
-                tmp.clear();
-            }
-            return;
+        if (type == 1) {
+            return dirBits;
         }
 
-        if (c[2] == 4) {
-            for (int d = 0; d < 4; d++) {
-                int nx = c[0] + dx[d];
-                int ny = c[1] + dy[d];
-                while (nx >= 0 && nx < n && ny >= 0 && ny < m && room[nx][ny] != 6) {
-                    if (room[nx][ny] == 0) {
-                        room[nx][ny] = -1;
-                        tmp.add(new int[]{nx, ny});
-                    }
-                    nx += dx[d];
-                    ny += dy[d];
-                }
-                nx = c[0] + dx[(d + 1) % 4];
-                ny = c[1] + dy[(d + 1) % 4];
-                while (nx >= 0 && nx < n && ny >= 0 && ny < m && room[nx][ny] != 6) {
-                    if (room[nx][ny] == 0) {
-                        room[nx][ny] = -1;
-                        tmp.add(new int[]{nx, ny});
-                    }
-                    nx += dx[(d + 1) % 4];
-                    ny += dy[(d + 1) % 4];
-                }
-                nx = c[0] + dx[(d + 2) % 4];
-                ny = c[1] + dy[(d + 2) % 4];
-                while (nx >= 0 && nx < n && ny >= 0 && ny < m && room[nx][ny] != 6) {
-                    if (room[nx][ny] == 0) {
-                        room[nx][ny] = -1;
-                        tmp.add(new int[]{nx, ny});
-                    }
-                    nx += dx[(d + 2) % 4];
-                    ny += dy[(d + 2) % 4];
-                }
-                bt(idx + 1, count - tmp.size());
-                for (int[] t : tmp) {
-                    room[t[0]][t[1]] = 0;
-                }
-                tmp.clear();
-            }
-            return;
+        if (type == 2) {
+            return new long[]{dirBits[0] | dirBits[2], dirBits[1] | dirBits[3]};
         }
 
-        if (c[2] == 5) {
-            int nx = c[0] + dx[0];
-            int ny = c[1] + dy[0];
-            while (nx >= 0 && nx < n && ny >= 0 && ny < m && room[nx][ny] != 6) {
-                if (room[nx][ny] == 0) {
-                    room[nx][ny] = -1;
-                    tmp.add(new int[]{nx, ny});
-                }
-                nx += dx[0];
-                ny += dy[0];
-            }
-            nx = c[0] + dx[1];
-            ny = c[1] + dy[1];
-            while (nx >= 0 && nx < n && ny >= 0 && ny < m && room[nx][ny] != 6) {
-                if (room[nx][ny] == 0) {
-                    room[nx][ny] = -1;
-                    tmp.add(new int[]{nx, ny});
-                }
-                nx += dx[1];
-                ny += dy[1];
-            }
-            nx = c[0] + dx[2];
-            ny = c[1] + dy[2];
-            while (nx >= 0 && nx < n && ny >= 0 && ny < m && room[nx][ny] != 6) {
-                if (room[nx][ny] == 0) {
-                    room[nx][ny] = -1;
-                    tmp.add(new int[]{nx, ny});
-                }
-                nx += dx[2];
-                ny += dy[2];
-            }
-            nx = c[0] + dx[3];
-            ny = c[1] + dy[3];
-            while (nx >= 0 && nx < n && ny >= 0 && ny < m && room[nx][ny] != 6) {
-                if (room[nx][ny] == 0) {
-                    room[nx][ny] = -1;
-                    tmp.add(new int[]{nx, ny});
-                }
-                nx += dx[3];
-                ny += dy[3];
-            }
-            bt(idx + 1, count - tmp.size());
-            for (int[] t : tmp) {
-                room[t[0]][t[1]] = 0;
-            }
-            tmp.clear();
+        if (type == 3) {
+            return new long[]{dirBits[0] | dirBits[1], dirBits[1] | dirBits[2], dirBits[2] | dirBits[3], dirBits[0] | dirBits[3]};
         }
+
+        if (type == 4) {
+            return new long[]{dirBits[0] | dirBits[1] | dirBits[2], dirBits[0] | dirBits[1] | dirBits[3], dirBits[0] | dirBits[2] | dirBits[3], dirBits[1] | dirBits[2] | dirBits[3]};
+        }
+
+        if (type == 5) {
+            return new long[]{dirBits[0] | dirBits[1] | dirBits[2] | dirBits[3]};
+        }
+
+        return new long[]{};
+    }
+
+    public static long getDirBit(int x, int y, int dir) {
+        long bit = pointToBit(x, y);
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+        while (nx >= 0 && nx < n && ny >= 0 && ny < m && room[nx][ny] != 6) {
+            if (room[nx][ny] == 0) {
+                bit |= pointToBit(nx, ny);
+            }
+            nx += dx[dir];
+            ny += dy[dir];
+        }
+        return bit;
+    }
+
+    public static long pointToBit(int x, int y) {
+        return (1L << (x * m + y));
     }
 }
