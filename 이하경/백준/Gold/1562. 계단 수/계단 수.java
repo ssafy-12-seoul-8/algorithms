c2947a0dc970239dc1093e7697e74050ab58fcad
puranium <63571947+puranium235@users.noreply.github.com>
2026-03-09 18:47:17 +0900

    [Gold I] Title: 계단 수, Time: 68 ms, Memory: 11520 KB -SsafyHub

--- 이하경/백준/Gold/1562. 계단 수/계단 수.java
+++ 이하경/백준/Gold/1562. 계단 수/계단 수.java
@@ -7,29 +7,29 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
 
         int DIV = 1_000_000_000;
-        int[][][] dp = new int[n][10][1 << 10];
-        for (int i = 1; i <= 9; i++) {
-            dp[0][i][1 << i] = 1;
+        int[][][] dp = new int[n][10][4]; // 0, 9 포함여부를 세번째에 저장
+        for (int i = 1; i < 9; i++) {
+            dp[0][i][0] = 1;
         }
+        dp[0][9][2] = 1;
 
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < 10; j++) {
-                for (int k = 0; k < (1 << 10); k++) {
-                    if (j < 9) {
-                        dp[i + 1][j + 1][k | (1 << (j + 1))] += dp[i][j][k];
-                        dp[i + 1][j + 1][k | (1 << (j + 1))] %= DIV;
-                    }
-                    if (j > 0) {
-                        dp[i + 1][j - 1][k | (1 << (j - 1))] += dp[i][j][k];
-                        dp[i + 1][j - 1][k | (1 << (j - 1))] %= DIV;
-                    }
-                }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < 9; j++) {
+                dp[i][j][0] = (dp[i - 1][j - 1][0] + dp[i - 1][j + 1][0]) % DIV;
+                dp[i][j][1] = (dp[i - 1][j - 1][1] + dp[i - 1][j + 1][1]) % DIV;
+                dp[i][j][2] = (dp[i - 1][j - 1][2] + dp[i - 1][j + 1][2]) % DIV;
+                dp[i][j][3] = (dp[i - 1][j - 1][3] + dp[i - 1][j + 1][3]) % DIV;
             }
+
+            dp[i][0][1] = (dp[i - 1][1][0] + dp[i - 1][1][1]) % DIV;
+            dp[i][0][3] = (dp[i - 1][1][2] + dp[i - 1][1][3]) % DIV;
+            dp[i][9][2] = (dp[i - 1][8][0] + dp[i - 1][8][2]) % DIV;
+            dp[i][9][3] = (dp[i - 1][8][1] + dp[i - 1][8][3]) % DIV;
         }
 
         int ans = 0;
-        for (int i = 0; i < 10; i++) {
-            ans += dp[n - 1][i][(1 << 10) - 1];
+        for (int i = 0; i <= 9; i++) {
+            ans += dp[n - 1][i][3];
             ans %= DIV;
         }
 
