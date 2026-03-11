cf7042fdb3623dea06f64dbe79e866634e45661d
puranium <63571947+puranium235@users.noreply.github.com>
2026-03-12 02:14:36 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 76 ms, Memory: 13800 KB -SsafyHub

---
+++ 이하경/백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+
+        System.out.println(s.substring(n - 5));
+    }
+}
