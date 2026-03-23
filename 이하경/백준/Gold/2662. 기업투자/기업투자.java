13b684c64eb60d0b79eee205514410673b41e8d1
puranium <63571947+puranium235@users.noreply.github.com>
2026-03-23 15:17:06 +0900

    [Gold II] Title: 기업투자, Time: 112 ms, Memory: 13440 KB -SsafyHub

--- 이하경/백준/Gold/2662. 기업투자/기업투자.java
+++ 이하경/백준/Gold/2662. 기업투자/기업투자.java
@@ -20,35 +20,34 @@ public class Main {
         }
 
         int[] dp = new int[n + 1];
-        ArrayList<Integer>[] track = new ArrayList[n + 1];
-
-        for (int i = 0; i <= n; i++) {
-            track[i] = new ArrayList<>();
-        }
+        int[][] prevs = new int[m][n + 1]; // 총 j원 투자했을때 i번 기업에 투자한 금액
 
         for (int i = 0; i < m; i++) {
             for (int j = n; j >= 0; j--) {
-                boolean pick = false;
                 for (int k = 1; k <= n; k++) {
                     if (j < k) {
                         break;
                     }
                     if (dp[j] < dp[j - k] + profit[i][k]) {
                         dp[j] = dp[j - k] + profit[i][k];
-                        track[j] = new ArrayList<>();
-                        track[j].addAll(track[j - k]);
-                        track[j].add(k);
-                        pick = true;
+                        prevs[i][j] = k;
                     }
-                }
-                if (!pick) {
-                    track[j].add(0);
                 }
             }
         }
 
         sb.append(dp[n]).append("\n");
-        for (int i : track[n]) {
+
+        int cost = n;
+        int company = m - 1;
+        int[] ans = new int[m];
+        while (company >= 0) {
+            ans[company] = prevs[company][cost];
+            cost -= prevs[company][cost];
+            company -= 1;
+        }
+
+        for (int i : ans) {
             sb.append(i).append(" ");
         }
         System.out.println(sb);
