e95171097d5c66f457dfec289a73f12b9dbfa934
sybang97 <sybang98@gmail.com>
2026-03-27 23:48:59 +0900

    [Gold V] Title: 전깃줄, Time: 184 ms, Memory: 18124 KB -SsafyHub

---
+++ 방승윤/백준/Gold/2565. 전깃줄/전깃줄.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[][] arr = new int[n][2];
+
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr, (o1, o2) -> o1[0] - o2[0]);
+		int[] dp = new int[n];
+		int max = 0;
+
+		for (int i = 0; i < n; i++) {
+			dp[i] = 1;
+
+			for (int j = 0; j < i; j++) {
+				if (arr[i][1] > arr[j][1]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+
+			max = Math.max(max, dp[i]);
+		}
+
+		System.out.println(n - max);
+	}
+}
