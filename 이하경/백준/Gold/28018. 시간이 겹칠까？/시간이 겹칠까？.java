52bf6c2aa1c71f5f42d9614d78c1f9353dc759a6
puranium <63571947+puranium235@users.noreply.github.com>
2026-04-01 17:56:54 +0900

    [Gold V] Title: 시간이 겹칠까?, Time: 376 ms, Memory: 61000 KB -SsafyHub

--- 이하경/백준/Gold/28018. 시간이 겹칠까？/시간이 겹칠까？.java
+++ 이하경/백준/Gold/28018. 시간이 겹칠까？/시간이 겹칠까？.java
@@ -8,39 +8,26 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
+        int[] diff = new int[1_000_002];
+        int[] sum = new int[1_000_002];
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            pq.add(new int[]{ start, 1 });
-            pq.add(new int[]{ end + 1, -1 });
+            diff[start] += 1;
+            diff[end + 1] -= 1;
+        }
+
+        for (int i = 1; i < diff.length; i++) {
+            sum[i] = sum[i - 1] + diff[i];
         }
 
         int q = Integer.parseInt(br.readLine());
-        int[][] queries = new int[q][3];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < q; i++) {
-            queries[i][0] = i;
-            queries[i][1] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(queries, Comparator.comparingInt(a -> a[1]));
-
-        int count = 0;
-        for (int i = 0; i < q; i++) {
-            while (!pq.isEmpty() && pq.peek()[0] <= queries[i][1]) {
-                count += pq.poll()[1];
-            }
-
-            queries[i][2] = count;
-        }
-
-        Arrays.sort(queries, Comparator.comparingInt(a -> a[0]));
-
-        for (int i = 0; i < q; i++) {
-            sb.append(queries[i][2]).append("\n");
+            int time = Integer.parseInt(st.nextToken());
+            sb.append(sum[time]).append("\n");
         }
 
         System.out.println(sb);
