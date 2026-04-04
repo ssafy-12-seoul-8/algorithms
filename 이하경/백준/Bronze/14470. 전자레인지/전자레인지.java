ee069a585f5de858c932c73d7391b0b63c4030ea
puranium <63571947+puranium235@users.noreply.github.com>
2026-04-05 01:38:52 +0900

    [Bronze IV] Title: 전자레인지, Time: 64 ms, Memory: 11468 KB -SsafyHub

---
+++ 이하경/백준/Bronze/14470. 전자레인지/전자레인지.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+        int e = Integer.parseInt(br.readLine());
+
+        System.out.println(a < 0 ? -a * c + d + b * e : (b - a) * e);
+    }
+}
