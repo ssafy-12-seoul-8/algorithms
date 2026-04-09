cefd55a401e735e7c9bf280e1b8991f10c4898d3
puranium <63571947+puranium235@users.noreply.github.com>
2026-04-09 13:21:22 +0900

    [Gold IV] Title: 고냥이, Time: 92 ms, Memory: 14192 KB -SsafyHub

--- 이하경/백준/Gold/16472. 고냥이/고냥이.java
+++ 이하경/백준/Gold/16472. 고냥이/고냥이.java
@@ -8,39 +8,31 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         String s = br.readLine();
         int len = s.length();
+
         int[] cha = new int[len];
         for (int i = 0; i < len; i++) {
             cha[i] = s.charAt(i) - 'a';
         }
-
         int[] cnt = new int[26];
-        int count = 0;
+        int kind = 0;
+        int left = 0;
         int max = 0;
-        int l = 0;
-        int r = -1;
-        while (r < len - 1) {
-            while (r < len - 1 && count < n) {
-                r++;
-                cnt[cha[r]]++;
-                if (cnt[cha[r]] == 1) {
-                    count++;
-                }
-                max = Math.max(max, r - l + 1);
+
+        for (int i = 0; i < len; i++) {
+            cnt[cha[i]]++;
+            if (cnt[cha[i]] == 1) {
+                kind++;
             }
 
-            while (r < len - 1 && count == n && cnt[cha[r + 1]] != 0) {
-                r++;
-                cnt[cha[r]]++;
-                max = Math.max(max, r - l + 1);
+            while (kind > n) {
+                cnt[cha[left]]--;
+                if (cnt[cha[left]] == 0) {
+                    kind--;
+                }
+                left++;
             }
 
-            while (count == n) {
-                cnt[cha[l]]--;
-                if (cnt[cha[l]] == 0) {
-                    count--;
-                }
-                l++;
-            }
+            max = Math.max(max, i - left + 1);
         }
 
         System.out.println(max);
